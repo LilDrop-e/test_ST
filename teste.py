47c7b1b94fd2aecd890e38099419fdd7c9da2976
LilDrop-e <pxaum21@gmail.com>
2025-05-23 10:58:52 -0300

    Update hup_hup³.py

--- teste.py
+++ teste.py
@@ -5,5 +5,13 @@ st.write("""
 # Gostou do pai?
 Dá um alo na dm bb
 """)
-
-st.image("https://ideogram.ai/assets/image/lossless/response/xd9F_8vqTlukcjmUtpFFEQ", caption="Sexy sendo vulgar!", width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto", *, use_container_width=False)
+st.image(
+    "https://ideogram.ai/assets/image/lossless/response/xd9F_8vqTlukcjmUtpFFEQ",
+    caption="Sexy sendo vulgar!",
+    width=None,
+    use_column_width=None,
+    clamp=False,
+    channels="RGB",
+    output_format="auto",
+    use_container_width=False
+)
