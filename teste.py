6c7f0fc21c64e53b14b873bf88721ce194810ce2
LilDrop-e <pxaum21@gmail.com>
2025-05-23 10:45:09 -0300

    Update hup hup.py

--- teste.py
+++ teste.py
@@ -6,4 +6,4 @@ st.write("""
 Dá um alo na dm bb
 """)
 
-st.image("johnny_bravo_sensualizando.jpg", caption="Sexy sendo vulgar")
+st.image("johnny_bravo_sensualizando.jpg", caption="Sexy sendo vulgar!", width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto", *, use_container_width=False))
